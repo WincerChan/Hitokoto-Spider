1b25597cc962e3e646891bb9130d4075c95bfa50
WincerChan <wincerchan@gmail.com>
2018-07-27 17:53:52 +0800

    Add orm model for hitokoto

--- src/database.py
+++ src/database.py
@@ -1,17 +1,17 @@
 import os
 import reprlib
 from re import sub
-from sqlalchemy import create_engine
-from sqlalchemy import exc
 
+from sqlalchemy import create_engine, exc, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, exc as orm_exc
 from xxhash import xxh64
-import yaml
+from yaml import load as yaml_load
 
 from progress import flush, write
 
-
 with open('../config.yml') as fp:
-    config = yaml.load(fp)
+    config = yaml_load(fp)
 
 engine = create_engine(
     'mysql+pymysql://{}:{}@127.0.0.1:{}/hitokoto?charset=utf8'.format(
@@ -20,39 +20,70 @@ engine = create_engine(
         config['mysql']['port']))
 
 
-COUNTER = engine.execute("SELECT COUNT(id) FROM main;").fetchone()[0]
+Base = declarative_base()
+
+DBsession = sessionmaker(bind=engine)
 
 
-def get_name(url):
-    return url.split('.')[1]
+class HitokotoInfo(Base):
+    __tablename__ = 'main'
+
+    id = Column(Integer, primary_key=True)
+    hitokoto = Column(String(300))
+    source = Column(String(64))
+    origin = Column(String(12))
+
+    def __repr__(self):
+        t = tuple(vars(self).values())
+        fmt = '<Hitokoto(id={}, hitokoto={}, source={}, origin={})>'
+        return fmt.format(*t)
+
+    def __str__(self):
+        reprlib.aRepr.maxstring = 55
+        return reprlib.repr(self.__repr__())
 
 
-def clear():
-    print()
-    if COUNTER % 100 == 0:
-        os.system("clear")
+session = DBsession()
+AMOUNT = session.query(HitokotoInfo.id).count()
 
 
 def fmt_data(c, url):
     hitokoto_tmp = c.get('hitokoto') or c.get('text') or c.get('HITO')
     source = c.get('source') or c.get('from') or c.get('SOURCE')
     hitokoto = sub(r'[\xa0-\xad]', '', hitokoto_tmp)
-    origin = get_name(url)
+    origin = url.split('.')[1]
+    # 去除一些无用的字符
     fmt_hitokoto = sub(r'[,，。.“” …！、\!\?：’；\\‘？「/」—-♬《》⋯『』（）]', '', hitokoto)
-    id = xxh64(fmt_hitokoto).intdigest()
+    id_ = xxh64(fmt_hitokoto).intdigest()
     if (source and origin):
-        insert_data(id, hitokoto, source, origin)
+        record = HitokotoInfo(id=id_, hitokoto=hitokoto,
+                              source=source, origin=origin)
+        insert_data(record)
 
 
-def insert_data(*data):
-    global COUNTER
+def insert_data(record):
+    global AMOUNT
+
+    def clear():
+        print()
+        if AMOUNT % 100 == 0:
+            os.system("clear")
+
     try:
-        engine.execute("INSERT INTO main VALUES {};".format(data))
-        COUNTER += 1
+        session.add(record)
+        session.commit()
+    except orm_exc.FlushError:
+        fmt = '已重复（{}）:{}'
+        print(fmt.format(AMOUNT, record))
+        session.rollback()
     except exc.IntegrityError:
-        print('已重复（%d）：%s' %
-              (COUNTER, reprlib.repr(str(data[0])+'，'+data[1])), end="")
+        fmt = '已重复（{}）:{}'
+        print(fmt.format(AMOUNT, record))
+        session.rollback()
+    except Exception as e:
+        print(e)
+        exit()
     else:
-        print('已插入（%d）：%s' %
-              (COUNTER, reprlib.repr(str(data[0])+'，'+data[1])), end="")
+        fmt = '已插入（{}）：{}'
+        print(fmt.format(AMOUNT, record))
     clear()
