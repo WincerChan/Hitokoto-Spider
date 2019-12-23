e14260fa29bdfd69f529fbed5b3d3314317cd399
WincerChan <WincerChan@gmail.com>
2019-12-23 06:06:00 +0100

    :hammer: Refactor usepython 3.8

---
+++ spider/database.py
@@ -0,0 +1,77 @@
+import os
+import reprlib
+from re import sub
+from typing import Any, Dict, Optional, Tuple
+
+from pony.orm import (Database, Json, PrimaryKey, Required,
+                      TransactionIntegrityError, commit, core, db_session, dbapiprovider)
+from xxhash import xxh64
+from yaml import SafeLoader
+from yaml import load as yaml_load
+
+with open('./config.yml') as fp:
+    config = yaml_load(fp, Loader=SafeLoader)
+
+db = Database()
+Entity: core.EntityMeta = db.Entity
+
+db.bind(provider='postgres', host='127.0.0.1',
+        user=config['postgres']['user'],
+        passwd=config['postgres']['password'], database='api')
+
+reprlib.aRepr.maxstring = 55
+
+PGINT64 = 2 ** 63 - 1
+
+
+class Helper:
+    amount = 0
+
+
+class Hitokoto(Entity):
+    """
+    ORM 中的表结构，这里的变量名即存放进数据库的列名
+    不能为这个 class 重写 str 或 repr 方法
+    因为 pony 存放的时候会调用这个方法
+    """
+    id = PrimaryKey(int, size=64)
+    length = Required(int)
+    info = Required(Json)
+    origin = Required(str)
+
+
+# 初始化数据库，如果表不存在就建表，存在就拿到数据条数
+db.generate_mapping(create_tables=True)
+with db_session:
+    Helper.amount = Hitokoto.select(lambda p: p.id).count()
+
+
+def fmt_data(c: Dict[str, str], url: str) -> None:
+    hitokoto_tmp = c.get('hitokoto', '') or c.get(
+        'text', '') or c.get('HITO', '')
+    source = c.get('source') or c.get('from') or c.get('SOURCE')
+    hitokoto = sub(r'[\xa0-\xad]', '', hitokoto_tmp)
+    origin = url.split('.')[1]
+    # 去除一些无用字符，避免无用字符对计算 hash 产生的影响
+    fmt_hitokoto = sub(
+        r'[,，。.“” …！、\!\?：’；\\‘？「/」—-♬《》⋯『』（）]', '', hitokoto)
+    id_ = xxh64(fmt_hitokoto).intdigest() - PGINT64
+    source and origin and insert_data(id_, hitokoto, source, origin)
+
+
+@db_session
+def insert_data(*record: Tuple[int, str, str, str]) -> None:
+    # 这里用事务包起来，方便错出后回退
+    try:
+        Hitokoto(id=record[0], info=dict(hitokoto=record[1], source=record[2]),
+                 length=len(record[1]), origin=record[3])
+        commit()
+    except TransactionIntegrityError:
+        has_duplicated = f'已重复（{Helper.amount}）：{record[0], record[1]}'
+        print(reprlib.repr(has_duplicated))
+    except Exception as e:
+        print(e)
+    else:
+        has_inserted = f'已插入（{Helper.amount}）：{record[0], record[1]}'
+        Helper.amount += 1
+        print(reprlib.repr(has_inserted))
--- spider/download.py
+++ spider/download.py
@@ -4,8 +4,8 @@ import string
 import aiohttp
 import json
 
-from database import fmt_data, config
-from progress import main as progress_main
+from spider.database import fmt_data, config
+from spider.progress import main as progress_main
 
 
 MAX_CONNECTION = config.get('pool_connection')
@@ -21,27 +21,12 @@ async def down(url):
                 cont = await resp.text()
                 fmt_data(json.loads(cont), url)
             except (asyncio.TimeoutError, aiohttp.ClientError):
-                print('The connect is timeout, try a new connect.')
+                print('连接已超时，尝试新建另一连接。')
                 continue
 
 
-def main(urls):
-    loop = asyncio.get_event_loop()
+async def main(urls):
+    urls = config.get('urls')
     to_do = [down(url) for url in urls]
     # 这里需要把进度条加入事件循环
-    to_do.append(progress_main())
-    wait_coro = asyncio.wait(to_do)
-    res, _ = loop.run_until_complete(wait_coro)
-    loop.close()
-    return len(res)
-
-
-if __name__ == '__main__':
-    urls = config.get('urls')
-    try:
-        if urls:
-            main(urls)
-        else:
-            print("请在配置文件中填入网址。")
-    except KeyboardInterrupt:
-        print("Good Bye!\033[?25h")
+    await asyncio.gather(*to_do, progress_main())
