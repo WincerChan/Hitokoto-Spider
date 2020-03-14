514f756f9d1456856a99cb6ccb67be15f5f35014
WincerChan <WincerChan@gmail.com>
2020-03-14 06:34:00 +0100

    :hammer: Refactor download function

--- spider/download.py
+++ spider/download.py
@@ -1,29 +1,29 @@
 import asyncio
-import json
-
-import aiohttp
+from typing import List, Dict
 
 from spider.database import fmt_data, config
 from spider.progress import main as progress_main
+from requests.exceptions import ReadTimeout, ConnectTimeout
 from requests_html import AsyncHTMLSession, TimeoutError, HTMLResponse
 
 MAX_CONNECTION = config.get('alive_connection')
-ASS = AsyncHTMLSession(loop=asyncio.get_event_loop(), workers=MAX_CONNECTION)
+ASS = AsyncHTMLSession()
 
 
 async def down(url: str):
-    timeout = aiohttp.ClientTimeout(connect=0.3)
     while True:
         try:
-            resp: HTMLResponse = await ASS.get(url, timeout=timeout)
+            resp: HTMLResponse = await ASS.get(url, timeout=0.3)
             conn = resp.json()
             fmt_data(conn, url)
-        except TimeoutError:
+        except (TimeoutError, ReadTimeout, ConnectTimeout):
             print("连接已超时，尝试新建另一连接。")
 
 
-async def main():
-    urls = config.get('urls')
+async def main(urls: List[Dict[str, int]]):
+    # 这里必须重新使用 loop 初始化 ASS
+    global ASS
+    ASS = AsyncHTMLSession(loop=asyncio.get_event_loop(), workers=MAX_CONNECTION)
     to_do = list()
     for item in urls:
         to_do.extend(
