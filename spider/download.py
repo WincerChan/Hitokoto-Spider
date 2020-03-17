59c01ac7a0368c9ab1b81c89aaaa46b4d8383176
WincerChan <WincerChan@gmail.com>
2020-03-17 06:38:00 +0100

    :hammer: Refactor classmethod to instancemethod

--- spider/download.py
+++ spider/download.py
@@ -12,57 +12,29 @@ ASS = AsyncHTMLSession()
 
 
 class Download:
-    max_connection = config.get('alive_connection')
-    session = AsyncHTMLSession()
-    instance = None
+    def __new__(cls, urls: List[Dict[str, int]], *args, **kwargs):
+        if not hasattr(cls, '_instance'):
+            cls._instance = super().__new__(cls, *args, **kwargs)
+        # setattr(cls._instance, 'urls', urls)
+        return cls._instance
 
-    def __new__(cls, *args, **kwargs):
-        if not cls.instance:
-            cls.instance = super().__new__(cls, *args, **kwargs)
-        return cls.instance
-
-    @classmethod
-    async def _do_down(cls, url: str):
+    async def _do_down(self, url: str):
         while True:
             try:
-                resp: HTMLResponse = await cls.session.get(url, timeout=0.3)
+                resp: HTMLResponse = await self.session.get(url, timeout=0.3)
                 conn = resp.json()
                 fmt_data(conn, url)
             except (TimeoutError, ReadTimeout, ConnectTimeout):
                 print("连接已超时，尝试新建另一连接。")
 
-    @classmethod
-    def _copy_urls(cls, item: Dict[str, int]):
+    def _copy_urls(self, item: Dict[str, int]):
         url, connections = item.get('url'), item.get('connection', 1)
-        return [cls._do_down(url) for _ in range(connections)]
+        return [self._do_down(url) for _ in range(connections)]
 
-    @classmethod
-    async def __call__(cls, urls: List[Dict[str, int]]):
-        # 这里由于 cls.session 初始化是在 import 的时候就初始化了，并非在 asyncio.run 的时候
-        # 因此需要在这里重新初始化 loop
-        cls.session = AsyncHTMLSession(loop=asyncio.get_event_loop(), workers=cls.max_connection)
-        todo = reduce(lambda c1, c2: c1 + c2, [cls._copy_urls(item) for item in urls])
+    async def __call__(self, urls: List[Dict[str, int]]):
+        self.session = AsyncHTMLSession(
+            loop=asyncio.get_event_loop(),
+            workers=config.get('alive_connection')
+        )
+        todo = reduce(lambda c1, c2: c1 + c2, [self._copy_urls(item) for item in urls])
         await asyncio.gather(*todo, progress_main())
-
-# async def down(url: str):
-#     while True:
-#         try:
-#             resp: HTMLResponse = await ASS.get(url, timeout=0.3)
-#             conn = resp.json()
-#             fmt_data(conn, url)
-#         except (TimeoutError, ReadTimeout, ConnectTimeout):
-#             print("连接已超时，尝试新建另一连接。")
-
-#
-# def copy_urls(item: Dict[str, int]):
-#     url, connections = item.get('url'), item('connection', 1)
-#     return [down(url) for _ in range(connections)]
-
-#
-# async def main(urls: List[Dict[str, int]]):
-#     # 这里必须重新使用 loop 初始化 ASS
-#     global ASS
-#     ASS = AsyncHTMLSession(loop=asyncio.get_event_loop(), workers=MAX_CONNECTION)
-#     todo = reduce(lambda c1, c2: c1 + c2, [copy_urls(item) for item in urls])
-#     # 这里需要把进度条也加入事件循环
-#     await asyncio.gather(*todo, progress_main())
