321a7a507d2be4283892b59d0bee172f3ff9075f
WincerChan <WincerChan@gmail.com>
2020-03-23 17:51:00 +0100

    :wrench: Update database tool and config

--- run.py
+++ run.py
@@ -2,14 +2,16 @@ import asyncio
 from typing import List, Dict
 
 from spider.download import Download
-from spider.database import config
+from spider.config import FrozenConfig
 
 if __name__ == '__main__':
-    urls: List[Dict[str, int]] = config.get('urls')
+    urls = FrozenConfig().urls
     try:
-        if urls:
-            asyncio.run(Download(urls))
+        if urls and urls[0]:
+            asyncio.run(Download())
         else:
             print("请在配置文件中填入网址。")
     except KeyboardInterrupt:
         print("Good Bye!\033[?25h")
+    finally:
+        print("\33[?25h", end="")
--- spider/download.py
+++ spider/download.py
@@ -4,30 +4,35 @@ from typing import List
 from requests.exceptions import ReadTimeout, ConnectTimeout
 from requests_html import AsyncHTMLSession, TimeoutError, HTMLResponse
 
-from spider.database import fmt_data, config
+from spider.database import DataFactory
+from spider.config import FrozenConfig
 from spider.progress import Progress
 
 
 class Download:
-    def __new__(cls, urls: List[str], *args, **kwargs):
+    _config = FrozenConfig()
+
+    def __new__(cls, *args, **kwargs):
         if not hasattr(cls, '_instance'):
             cls._instance = super().__new__(cls, *args, **kwargs)
-            cls._instance.urls = urls
+            cls._instance.urls = cls._config.urls
         return cls._instance.__call__()
 
     async def _do_down(self, url: str) -> None:
         while True:
             try:
-                resp: HTMLResponse = await self.session.get(url, timeout=0.3)
+                resp: HTMLResponse = await self.session.get(url, timeout=1.3)
                 conn = resp.json()
-                fmt_data(conn, url)
+                DataFactory.fmt_data(conn, url)
             except (TimeoutError, ReadTimeout, ConnectTimeout):
+                import traceback
+                traceback.print_exc()
                 print("连接已超时，尝试新建另一连接。")
 
     async def __call__(self):
         self.session = AsyncHTMLSession(
             loop=asyncio.get_event_loop(),
-            workers=config.get('alive_connection')
+            workers=self._config.alive_connection
         )
         todo = [self._do_down(url) for url in self.urls]
         await asyncio.gather(*todo, Progress.show())
